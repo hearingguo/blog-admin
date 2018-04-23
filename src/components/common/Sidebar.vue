e321b44ff1c0e1de71a46f3d94edb43aa576c925
HearingG <295391018@qq.com>
2018-04-24 07:41:27 +0800

    sidebar

--- src/components/common/Sidebar.vue
+++ src/components/common/Sidebar.vue
@@ -1,22 +1,33 @@
 <template>
   <el-aside class="sidebar" width="200px">
     <el-menu
-      default-active="2"
-      class="el-menu-vertical">
+      class="el-menu-vertical"
+      default-active="currentPath"
+      unique-opened
+      router>
       <template v-for="(item, index) in $router.options.routes">
-        <el-submenu :index="index" v-if="item.meta.show && !item.meta.leaf && item.children" :key="index">
+        <el-submenu
+          v-if="item.meta.show && !item.meta.leaf && item.children"
+          :index="index"
+          :key="index">
           <template slot="title">
             <i class="el-icon-menu"></i>
             <span class="title">{{item.name}}</span>
           </template>
-          <el-menu-item v-for="child in item.children" :index="child.path" :key="child.path">
+          <el-menu-item
+            v-for="child in item.children"
+            :index="child.path"
+            :key="child.path">
             <i class="el-icon-menu"></i>
             <span class="text">
               {{child.name}}
             </span>
           </el-menu-item>
         </el-submenu>
-        <el-menu-item v-if="item.meta.show && item.meta.leaf && item.children" :index="item.children[0].path" :key="index">
+        <el-menu-item
+          v-if="item.meta.show && item.meta.leaf && item.children"
+          :index="item.children[0].path"
+          :key="index">
           <i class="el-icon-menu" ></i>
           <span>{{item.name}}</span>
         </el-menu-item>
@@ -27,14 +38,16 @@
 
 <script lang="ts">
 
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import { Route } from 'vue-router'
 
 @Component
 export default class Sidebar extends Vue {
 
-  mounted () {
-    // @ts-ignore
-    console.log(this.$router.options.routes)
+  private currentPath: string = ''
+
+  private created (): void {
+    this.currentPath = this.$route.path
   }
   
 }
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import Router from 'vue-router'
+import Router, { RouteConfig } from 'vue-router'
 import login from '../components/page/Login.vue'
 import index from '../components/page/Index.vue'
 import article from '../components/page/article/Article.vue'
@@ -10,56 +10,61 @@ import Info from '../components/page/info/Info.vue'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: '文章管理',
-      component: index,
-      meta: { leaf: false, show: true },
-      children: [
-        { path: '/article/index', component: article, name: '文章列表' },
-        { path: '/article/release', component: release, name: '文章发布' }
-      ]
-    },
-    {
-      path: '/',
-      name: '文章标签',
-      component: index,
-      meta: { leaf: true, show: true },
-      children: [
-        { path: '/tags', component: Tags, name: '文章标签' }
-      ]
-    },
-    {
-      path: '/',
-      name: '评论',
-      component: index,
-      meta: { leaf: true, show: true },
-      children: [
-        { path: '/comment', component: Comment, name: '评论' }
-      ]
-    },
-    {
-      path: '/',
-      name: '信息管理',
-      component: index,
-      meta: { leaf: true, show: true },
-      children: [
-        { path: '/info', component: Info, name: '信息管理' }
-      ]
-    },
-    {
-      path: '/',
-      name: 'login',
-      component: login,
-      meta: { leaf: true, show: false }
-    },
-    {
-      path: '/home',
-      name: 'home',
-      component: index,
-      meta: { leaf: true, show: false }
-    }
-  ]
+const routes: RouteConfig[] = [
+  {
+    path: '/',
+    name: 'login',
+    component: login,
+    meta: { leaf: true, show: false }
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: index,
+    meta: { leaf: true, show: false }
+  },
+  {
+    path: '/',
+    name: '文章管理',
+    component: index,
+    meta: { leaf: false, show: true },
+    children: [
+      { path: '/article/index', component: article, name: '文章列表' },
+      { path: '/article/release', component: release, name: '文章发布' }
+    ]
+  },
+  {
+    path: '/',
+    name: '文章标签',
+    component: index,
+    meta: { leaf: true, show: true },
+    children: [
+      { path: '/tags', component: Tags, name: '文章标签' }
+    ]
+  },
+  {
+    path: '/',
+    name: '评论',
+    component: index,
+    meta: { leaf: true, show: true },
+    children: [
+      { path: '/comment', component: Comment, name: '评论' }
+    ]
+  },
+  {
+    path: '/',
+    name: '信息管理',
+    component: index,
+    meta: { leaf: true, show: true },
+    children: [
+      { path: '/info', component: Info, name: '信息管理' }
+    ]
+  }
+]
+
+const router: Router = new Router({
+  mode: 'history',
+  routes
 })
+
+export default router
